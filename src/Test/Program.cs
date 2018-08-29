d333b7f4318e7d7047d975e0796a4bf626747fc5
luqingfei <luqingfei@126.com>
2018-08-29 11:13:17 +0800

    new

---
+++ src/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection()) //创建连接
+            {
+                using (var channel = connection.CreateModel()) //创建信道
+                {
+                    
+
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
